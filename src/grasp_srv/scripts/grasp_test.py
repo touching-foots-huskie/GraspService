4f35a0cef40903e6d1fc5ee0538edb04bfe67173
Harvey Chang <chnme40cs@gmail.com>
2020-09-03 01:56:43 +0000

    Fix bug in Grasp Client

--- src/grasp_srv/scripts/grasp_test.py
+++ src/grasp_srv/scripts/grasp_test.py
@@ -4,9 +4,9 @@ from __future__ import print_function
 
 import sys
 import rospy
-from grasp_srv.srv import *
-from grasp_srv.msg import *
-from geometry_msgs.msg import *
+import grasp_srv.srv
+import grasp_srv.msg
+import geometry_msgs.msg
 
 
 '''
@@ -15,16 +15,22 @@ Pose is a 1D list of 7 elements (x, y, z, qx, qy, qz, qw),
 def test_grasp_gen(model_name_list, object_pose_list):
     rospy.wait_for_service('grasp_gen')
     try:
-        grasp_gen = rospy.ServiceProxy('grasp_gen', GraspGen)
-        ObjectPoses object_poses()
+        grasp_gen = rospy.ServiceProxy('grasp_gen', grasp_srv.srv.GraspGen)
+        object_poses = grasp_srv.msg.ObjectPoses()
         for name, pose in zip(model_name_list, object_pose_list):
             object_poses.object_names.append(name)
-            Pose object_pose()
+            object_pose = geometry_msgs.msg.Pose()
             # set position
-            object_pose.poistion    = [pose[0], pose[1], pose[2]]
-            object_pose.orientation = [pose[3], pose[4], pose[5], pose[6]]
+            object_pose.position.x = pose[0]
+            object_pose.position.y = pose[1]
+            object_pose.position.z = pose[2]
+            object_pose.orientation.x = pose[3]
+            object_pose.orientation.y = pose[4]
+            object_pose.orientation.z = pose[5]
+            object_pose.orientation.w = pose[6]
             object_poses.object_poses.append(object_pose)
         resp = grasp_gen(object_poses)
+        print("Service Sent")
         return
 
     except rospy.ServiceException as e:
