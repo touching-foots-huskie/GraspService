a138054dabc4752b61ee1628f3f02ff4af71246f
Harvey Chang <503675973@qq.com>
2020-09-26 22:13:43 -0400

    Update grasp_visualizer.cpp

--- src/visualizer/src/grasp_visualizer.cpp
+++ src/visualizer/src/grasp_visualizer.cpp
@@ -65,8 +65,8 @@ GraspVisualizer::GraspVisualizer(QWidget* parent) : QWidget(parent) {
     visual_layout->addLayout(renderLayout);
     // test
     list_area = new QListWidget(this);
-    visual_layout->addWidget(list_area);
     connect(list_area, SIGNAL(itemActivated(QListWidgetItem)), this, SLOT(read_image(QListWidgetItem)));
+    visual_layout->addWidget(list_area);
     
     // Control Layout
     bt1_ = new QPushButton("Start", this);
