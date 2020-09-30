c536731383b4ed2f9a4681832ed81c410fc76711
Harvey Chang <503675973@qq.com>
2020-09-30 17:53:15 -0400

    Update type_gen.py

--- src/grasp_srv/scripts/type_gen.py
+++ src/grasp_srv/scripts/type_gen.py
@@ -8,8 +8,30 @@ if __name__ == "__main__":
     with open(name_file, "r") as f:
         name_dict = json.load(f)
     
-    for key, value in name_dict.items():
-        type_dict[value] = 1  # take box as default
+    for key, name in name_dict.items():
+        grasp_mode = dict()
+        if "box" in name or "cube" in name or "block" in name:
+            grasp_mode["basic_types"] = ["box"];
+            grasp_mode["block_list"] = [False, False, False, False, False, False, False, False, False, False, False, False];
+            grasp_mode["hard"] = False
+        elif "cup" in name or "can" in name:
+            grasp_mode["basic_types"] = ["can"];
+            grasp_mode["block_list"] = [False, False, False, False, False, False, False, False, False, False, False, False];
+            grasp_mode["hard"] = False
+        elif "plate" in name:
+            grasp_mode["basic_types"] = ["can"];
+            grasp_mode["block_list"] = [True, False, True, True, False, False, False, False, False, False, False, False];
+            grasp_mode["hard"] = False
+        elif "bottle" in name:
+            grasp_mode["basic_types"] = ["box"];
+            grasp_mode["block_list"] = [False, False, False, False, False, False, False, False, True, True, True, True];
+            grasp_mode["hard"] = False
+        else:
+            # default as box for now
+            grasp_mode["basic_types"] = ["box"];
+            grasp_mode["block_list"] = [False, False, False, False, False, False, False, False, False, False, False, False];
+            grasp_mode["hard"] = False
+        type_dict[name] = grasp_mode
 
     with open(type_file, "w") as f:
         json.dump(type_dict, f)
