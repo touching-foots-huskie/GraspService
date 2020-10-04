350170c43554e48a6bbf9f85359f49c8ed9507a9
Harvey Chang <503675973@qq.com>
2020-10-04 00:41:45 -0400

    Update geometry_grasp.cpp

--- src/grasp_srv/src/geometry_grasp.cpp
+++ src/grasp_srv/src/geometry_grasp.cpp
@@ -351,7 +351,9 @@ void box_grasp(MatrixArray& frame_array, VectorArray& position_array, std::strin
                         edge_vector = frame_ij * edge_vector;
                         Eigen::Vector3d size_vector(sizes[0], sizes[1], sizes[2]);
                         edge_vector = edge_vector.cwiseProduct(size_vector);
-                        if(edge_vector.norm() >= 0.12) break;
+                        double edge_len = edge_vector.norm();
+                        std::cout << edge_len << std::endl;
+                        if(edge_len >= finger_len) break;
                     }
 
                     position_array.push_back(pose_ij);
